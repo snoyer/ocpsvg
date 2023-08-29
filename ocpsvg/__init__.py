b8688027b37c6c6fbd37071fe9a28525e9d8103d
snoyer <noyer.stephane@gmail.com>
2023-08-29 07:00:14 +0400

    remove wildcard import

--- ocpsvg/__init__.py
+++ ocpsvg/__init__.py
@@ -1,1 +1,14 @@
-from .svg import *
+from .svg import (
+    ColorAndLabel,
+    DocumentInfo,
+    SvgPathCommand,
+    curve_to_svg_path,
+    edge_to_svg_path,
+    edges_from_svg_path,
+    face_to_svg_path,
+    faces_from_svg_path,
+    format_svg,
+    import_svg_document,
+    wire_to_svg_path,
+    wires_from_svg_path,
+)
